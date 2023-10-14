84870511e9f741ce146253e12a50f7fab291c483
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2023-10-14 16:06:13 -0300

    errorPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
-
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 
+import Error from './routes/Error/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/' ,
+    element: <App/> ,
+    errorElement: <Error/>,
+    children: []
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
